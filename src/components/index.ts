2faa44267698da3af8100afef539b6b632ae2593
Tomasz Stanisz <fremen1990@gmail.com>
2023-11-15 11:47:35 +0100

    :sparkles: Add Fox animation and Alert

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import {Loader} from "./Loader.tsx"
 import {Navbar} from "./Navbar.tsx"
 import {HomeInfo} from "./HomeInfo.tsx"
+import {Alert} from "./Alert.tsx"
 
-export {Loader, Navbar, HomeInfo};
+export {Loader, Navbar, HomeInfo, Alert};
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,18 @@
-import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import { ChangeEvent, FormEvent, Suspense, useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
 import emailjs from "@emailjs/browser";
+
+import { Fox } from "../models";
+import { Alert, Loader } from "../components";
+import { useAlert } from "../hooks/useAlert.ts";
 
 export const Contact = () => {
   const formRef = useRef<HTMLFormElement>(null); // Updated to specify HTMLFormElement
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const { showAlert, alert, hideAlert } = useAlert();
 
   const handleChange = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -15,28 +23,46 @@ export const Contact = () => {
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     setIsLoading(true);
+    setCurrentAnimation("hit");
 
     if (formRef.current) {
       emailjs
-        .send(
+        .sendForm(
           import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
           import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
           formRef.current, // Pass the form reference
           import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
         )
-        .then(
-          (result) => {
-            console.log(result.text);
-            // TODO  Handle success (reset form, show message, etc.)
-          },
-          (error) => {
-            console.log(error.text);
-            // TODO Handle errors show message, etc.)
-          }
-        )
-        .finally(() => {
-          setIsLoading(false);
+        .then((result) => {
+          console.log(result.text);
+          setTimeout(() => {
+            setCurrentAnimation("idle");
+            showAlert({
+              show: true,
+              text: "Message sent successfully!",
+              type: "success",
+            });
+            setIsLoading(false);
+            clearForm();
+          }, 4000);
+
+          setTimeout(() => {
+            hideAlert();
+          }, 7000);
+        })
+        .catch((error) => {
+          console.log(error.text);
+          showAlert({
+            show: true,
+            text: "Something went wrong, try again later!",
+            type: "danger",
+          });
           clearForm();
+          setCurrentAnimation("idle");
+          setIsLoading(false);
+          setTimeout(() => {
+            hideAlert();
+          }, 5000);
         });
     }
   };
@@ -45,11 +71,15 @@ export const Contact = () => {
     setForm({ name: "", email: "", message: "" });
   };
 
-  const handleFocus = () => {};
+  const handleFocus = () => {
+    setCurrentAnimation("walk");
+  };
   const handleBlur = () => {};
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {/*{alert.show && <Alert {...alert} />}*/}
+      { <Alert {...alert} />}
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
@@ -71,6 +101,7 @@ export const Contact = () => {
               onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
+              disabled={isLoading}
             />
           </label>
 
@@ -86,6 +117,7 @@ export const Contact = () => {
               onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
+              disabled={isLoading}
             />
           </label>
 
@@ -101,6 +133,7 @@ export const Contact = () => {
               onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
+              disabled={isLoading}
             />
           </label>
 
@@ -109,10 +142,26 @@ export const Contact = () => {
             className="btn"
             onFocus={handleFocus}
             onBlur={handleBlur}
+            disabled={isLoading}
           >
             {isLoading ? "Sending..." : "Send Message"}
           </button>
         </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas camera={{ position: [0, 0, 5] }}>
+          <directionalLight intensity={2} position={[0, 0, 1]} />
+          <ambientLight intensity={0.3} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
