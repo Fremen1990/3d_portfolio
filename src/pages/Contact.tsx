a3ec6bae86c66c33bd40f64bbb3b3dc7ac12a2e4
Tomasz Stanisz <fremen1990@gmail.com>
2023-11-15 09:18:11 +0100

    :memo: Implement Contact Form and emailjs submission

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,119 @@
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
 export const Contact = () => {
-    return (
-        <div>
-            <h1>Contact</h1>
-        </div>
-    )
-}
+  const formRef = useRef<HTMLFormElement>(null); // Updated to specify HTMLFormElement
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    if (formRef.current) {
+      emailjs
+        .send(
+          import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+          formRef.current, // Pass the form reference
+          import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            // TODO  Handle success (reset form, show message, etc.)
+          },
+          (error) => {
+            console.log(error.text);
+            // TODO Handle errors show message, etc.)
+          }
+        )
+        .finally(() => {
+          setIsLoading(false);
+          clearForm();
+        });
+    }
+  };
+
+  const clearForm = () => {
+    setForm({ name: "", email: "", message: "" });
+  };
+
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form
+          ref={formRef}
+          action=""
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col gap-7 mt-14"
+        >
+          <label htmlFor="" className="text-black font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label htmlFor="" className="text-black font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="john@email.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label htmlFor="" className="text-black font-semibold">
+            Message
+            <textarea
+              name="message"
+              rows={5}
+              className="textarea"
+              placeholder="Let me know about your project, needs or just say hi!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
